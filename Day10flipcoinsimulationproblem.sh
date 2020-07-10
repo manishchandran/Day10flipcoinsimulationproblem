eace4b6e00e39b2a3bab9a805d458f5f70fdfc41
MANISH <manishchandrashekharan@gmail.com>
2020-07-10 23:46:39 +0530

    use_case4 created

--- Day10flipcoinsimulationproblem.sh
+++ Day10flipcoinsimulationproblem.sh
@@ -1,26 +1,39 @@
 #!/bin/bash  
-#uc2
+#uc4
+
 Heads=0
 Tails=0
 flips=0
-while [ $flips ]
+while [ $flips -le 40 ]
 do
-	randomcheck=$(( RANDOM%2 ))
-	if [ $randomcheck -eq 1 ]
-	then
-		Heads=`expr $Heads + 1`
-	else
-		Tails=`expr $Tails + 1`
-	fi
-	flips=`expr $flips + 1`
-if [[ ($Heads -eq 10) || ($Tails -eq 10) ]]
+        randomcheck=$(( RANDOM%2 ))
+        if [ $randomcheck -eq 1 ]
+        then
+                Heads=`expr $Heads + 1`
+        else
+                Tails=`expr $Tails + 1`
+        fi
+        ((flips++))
+if [[ ($Heads -eq 21) || ($Tails -eq 21) ]]
 then 
-	break
+        if [ $Heads -eq 21 ]
+        then
+                echo "Heads won"
+                echo "Won by:"$Heads
+                break
+        elif [ $Tails -eq 21 ]
+	then
+                echo "Tails won"
+                echo "Won by:"$Tails 
+                break
+	elif [ $Heads -eq $Tails ]
+	then
+        	echo "It is a tie"
+	else
+		echo "Not working"
+	fi
 else
-	continue
+        continue
 fi
 done
-echo "No of times heads won:"$Heads 
-echo "No of times tails won:"$Tails
-
-
+echo $Heads $Tails
