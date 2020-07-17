64cb4ee9a16c2096716d4df87f0b3edfd1e50322
MANISH <manishchandrashekharan@gmail.com>
2020-07-18 00:00:50 +0530

    use_case2 created

--- Day10flipcoinsimulation.sh
+++ Day10flipcoinsimulation.sh
@@ -9,4 +9,27 @@ else
 	echo "TAILS IS THE WINNER"
 fi
 
+#uc2
+Heads=0
+Tails=0
+flips=0
+while [ $flips ]
+do
+	randomcheck=$(( RANDOM%2 ))
+	if [ $randomcheck -eq 1 ]
+	then
+		Heads=`expr $Heads + 1`
+	else
+		Tails=`expr $Tails + 1`
+	fi
+	flips=`expr $flips + 1`
+if [[ ($Heads -eq 10) || ($Tails -eq 10) ]]
+then 
+	break
+else
+	continue
+fi
+done
+echo "No of times heads won:"$Heads 
+echo "No of times tails won:"$Tails
 
