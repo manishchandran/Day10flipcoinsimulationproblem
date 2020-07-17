32558a9150ae49906b0ab93a74e6fd4ee78f8f6b
MANISH <manishchandrashekharan@gmail.com>
2020-07-18 00:02:07 +0530

    use_case3 created

--- Day10flipcoinsimulation.sh
+++ Day10flipcoinsimulation.sh
@@ -33,3 +33,33 @@ done
 echo "No of times heads won:"$Heads 
 echo "No of times tails won:"$Tails
 
+#uc3
+Heads=0
+Tails=0
+flips=0
+while [ $flips ]
+do
+	randomcheck=$(( RANDOM%2 ))
+	if [ $randomcheck -eq 1 ]
+	then
+		Heads=`expr $Heads + 1`
+	else
+		Tails=`expr $Tails + 1`
+	fi
+	flips=`expr $flips + 1`
+if [[ ($Heads -eq 21) || ($Tails -eq 21) ]]
+then 
+	if [ $Heads -eq 21 ]
+	then
+		echo "Heads won"
+		echo "Won by:"$Heads
+		break
+	else 
+		echo "Tails won"
+		echo "Won by:"$Tails 
+		break
+	fi
+else
+	continue
+fi
+done
